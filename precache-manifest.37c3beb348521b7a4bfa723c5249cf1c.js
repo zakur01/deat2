9d25d702dc7a19a3d86de92c9654dd448bf23435
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2020-07-22 00:10:40 +0600

    Updates

---
+++ precache-manifest.37c3beb348521b7a4bfa723c5249cf1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8eeae83f074dcfafe83074a60fbf7ae",
+    "url": "/husk/index.html"
+  },
+  {
+    "revision": "1c314aeafd7cc1cf6a2c",
+    "url": "/husk/static/css/main.07e5b9ab.chunk.css"
+  },
+  {
+    "revision": "f0ecaae86e8b5dbfde50",
+    "url": "/husk/static/js/2.f1b296c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/husk/static/js/2.f1b296c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c314aeafd7cc1cf6a2c",
+    "url": "/husk/static/js/main.eefeaf83.chunk.js"
+  },
+  {
+    "revision": "1f4a821d95f4a55ab5cd",
+    "url": "/husk/static/js/runtime-main.cbe39216.js"
+  },
+  {
+    "revision": "4dbc02c406656587744bcdc88f348e34",
+    "url": "/husk/static/media/back.4dbc02c4.jpg"
+  },
+  {
+    "revision": "a38e365345d9d37c72c68cb626525173",
+    "url": "/husk/static/media/ufo.a38e3653.gif"
+  }
+]);
